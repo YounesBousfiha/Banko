5c8986be0a8e53b19ab3102d84837fae34fabe23
Coderyounes <younesanas2021@gmail.com>
2025-09-15 16:41:10 +0100

    feat: add constrcutur & abstract methods

--- src/banque/domain/entity/Compte.java
+++ src/banque/domain/entity/Compte.java
@@ -2,4 +2,15 @@ package banque.domain.entity;
 
 public abstract class Compte {
 	
+	protected String code;
+	protected Double solde;
+	
+	public Compte( String code, Double solde) {
+		this.code = code;
+		this.solde = solde;
+	}
+	
+	public abstract void retiter();
+	public abstract Double calculerInteret();
+	public abstract void afficherDetails();
 } 
