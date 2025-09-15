a41b8daa780a1818a4847a2f945d197c98e22de4
Coderyounes <younesanas2021@gmail.com>
2025-09-15 16:42:20 +0100

    feat: implement abstract methods & add constructurs

--- src/banque/domain/entity/CompteEpargne.java
+++ src/banque/domain/entity/CompteEpargne.java
@@ -1,5 +1,37 @@
 package banque.domain.entity;
 
-public abstract class CompteEpargne extends Compte {
+public class CompteEpargne extends Compte {
+	
+	private Double tauxInteret;
+	public CompteEpargne(String code, Double solde, Double tauxInteret) {
+		super(code, solde);
+		this.tauxInteret = tauxInteret;
+	}
+	
+	
+	public Double getTauxInteret() {
+		return this.tauxInteret;
+	}
+	
+	public void setTauxInteret(Double newTaux) {
+		this.tauxInteret = newTaux;
+	}
+	@Override
+	public void retiter() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Double calculerInteret() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void afficherDetails() {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
