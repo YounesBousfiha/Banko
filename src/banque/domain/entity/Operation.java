db52c932dd3523a378d7fc08b7fd760a1b3be698
Coderyounes <younesanas2021@gmail.com>
2025-09-15 17:00:45 +0100

    feat: define Operation as abstract class

--- src/banque/domain/entity/Operation.java
+++ src/banque/domain/entity/Operation.java
@@ -1,5 +1,16 @@
 package banque.domain.entity;
 
+import java.time.LocalDate;
+
 public abstract class Operation {
+	protected String uuid;
+	protected LocalDate date;
+	protected Double montant;
+	
+	public Operation(String uuid, LocalDate date, Double montant) {
+		this.uuid = uuid;
+		this.date = date;
+		this.montant = montant;
+	}
 	
 }
