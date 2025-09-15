149ebcd30c623569260b92332e7ad7cab09c9bb3
Coderyounes <younesanas2021@gmail.com>
2025-09-15 17:01:13 +0100

    feat: define Retrait class & extends from Operation class

--- src/banque/domain/entity/Retrait.java
+++ src/banque/domain/entity/Retrait.java
@@ -1,3 +1,20 @@
 package banque.domain.entity;
 
-public class Retrait extends Operation {}
+import java.time.LocalDate;
+
+public class Retrait extends Operation {
+	private String destination;
+	
+	public Retrait(String uuid, LocalDate date, Double montant, String destination) {
+		super(uuid, date, montant);
+		this.destination = destination;
+	}
+	
+	public String getDestination() {
+		return this.destination;
+	}
+	
+	public void setDestination(String newDest) {
+		this.destination = newDest;
+	}
+}
