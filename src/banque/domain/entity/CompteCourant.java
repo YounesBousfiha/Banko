15b619272a35acdfea31822c32dfe016e6f6c29a
Coderyounes <younesanas2021@gmail.com>
2025-09-15 16:41:56 +0100

    feat: implement the abstract methods with empty body & add constrcutr

--- src/banque/domain/entity/CompteCourant.java
+++ src/banque/domain/entity/CompteCourant.java
@@ -1,5 +1,38 @@
 package banque.domain.entity;
 
-public abstract class CompteCourant extends Compte {
+public class CompteCourant extends Compte {
+	
+	private Double decouvert;
+	
+	public CompteCourant(String code, Double solde, Double decouvert) {
+		super(code, solde);
+		this.decouvert = decouvert;
+	} 
+	
+	public Double getDecouvert() {
+		return this.decouvert;
+		}
+	
+	public void setDecouvert(Double newDecouvert) {
+		this.decouvert = newDecouvert;
+	}
+	
+	@Override
+	public void retiter() {
+		// TODO solde ne doit pas etre inferieur a decourvert
+		
+	}
+
+	@Override
+	public Double calculerInteret() {
+		// TODO Auto-generated method stub
+		return 0.0;
+	}
+
+	@Override
+	public void afficherDetails() {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
