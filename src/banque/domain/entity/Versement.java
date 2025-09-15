3ee6286e0ab051e408f464269807a95d09fcad23
Coderyounes <younesanas2021@gmail.com>
2025-09-15 17:01:47 +0100

    feat: define versement class & extends from operation classes

--- src/banque/domain/entity/Versement.java
+++ src/banque/domain/entity/Versement.java
@@ -1,3 +1,20 @@
 package banque.domain.entity;
 
-public class Versement extends Operation {}
+import java.time.LocalDate;
+
+public class Versement extends Operation {
+	private String source;
+	
+	public Versement(String uuid, LocalDate date, Double montant, String source) {
+		super(uuid, date, montant);
+		this.source = source;
+	}
+	
+	public String getSource() {
+		return this.source;
+	}
+	
+	public void setSource(String newSource) {
+		this.source = newSource;
+	}
+}
