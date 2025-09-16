efd08ad188deb1a10bffdb5748be2312e576cd27
Coderyounes <younesanas2021@gmail.com>
2025-09-16 14:09:31 +0100

    Feat: Create Compte Controller

--- src/banque/presentation/controller/CompteController.java
+++ src/banque/presentation/controller/CompteController.java
@@ -1,6 +1,9 @@
 package banque.presentation.controller;
 
 import banque.application.service.CompteService;
+import banque.presentation.dto.CompteCourantDTO;
+import banque.presentation.dto.CompteDTO;
+import banque.presentation.dto.CompteEpargneDTO;
 
 public class CompteController {
 	
@@ -8,5 +11,28 @@ public class CompteController {
 	
 	public CompteController(CompteService compteService) {
 		this.compteService = compteService;
-	} 
+	}
+	
+	// Create Account Method
+	public void CreateAccount(String code, String type, Double solde) {
+		CompteDTO compte = null;
+		
+		switch(type) {
+		case "courant":
+			compte = new CompteCourantDTO(code, solde, 500.00);
+			break;
+		case "epargne":
+			compte = new CompteEpargneDTO(code, solde, 2.00);
+			break;
+		default:
+			System.out.println("Invalid choix !");
+		}
+		
+		CompteDTO newCompte = this.compteService.createAccount(compte);
+	}
+	
+	// getSolde method
+	public Double getSolde(String code) {
+		return this.compteService.getSolde(code);
+	}
 }
