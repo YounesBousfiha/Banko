f8e761128833c449a243ad8eb104513f67ab5f84
Coderyounes <younesanas2021@gmail.com>
2025-09-16 12:21:57 +0100

    Feat: add CompteService layer

--- src/banque/application/service/CompteService.java
+++ src/banque/application/service/CompteService.java
@@ -1,6 +1,9 @@
 package banque.application.service;
 
+import banque.application.mapper.CompteMapper;
+import banque.domain.entity.Compte;
 import banque.domain.repository.CompteRepository;
+import banque.presentation.dto.CompteDTO;
 
 public class CompteService {
 	
@@ -8,5 +11,22 @@ public class CompteService {
 	
 	public CompteService(CompteRepository compteRepository) {
 		this.compteRepository = compteRepository;
+	}
+		
+	// TODO: Validations
+	public CompteDTO createAccount(CompteDTO compteDto) {
+		
+		Compte compte = CompteMapper.toEntity(compteDto);
+		
+		this.compteRepository.save(compte);
+		
+		return CompteMapper.toDTO(compte);
+	}
+	
+	
+	
+	public Double getSolde(String code) {
+		Compte compte = this.compteRepository.findByCode(code);
+		return compte.getSolde();
 	}
 }
