f182df1c10c6ec42b7f82ae593a8e9bed0ea4d5a
Coderyounes <younesanas2021@gmail.com>
2025-09-16 18:59:33 +0100

    feat: update & add validations to CompteService

--- src/banque/application/service/CompteService.java
+++ src/banque/application/service/CompteService.java
@@ -2,7 +2,12 @@ package banque.application.service;
 
 import banque.application.mapper.CompteMapper;
 import banque.domain.entity.Compte;
+import banque.domain.entity.CompteEpargne;
+import banque.domain.exception.CompteNotFound;
+import banque.domain.exception.DuplicateAccount;
+import banque.domain.exception.InvalidAccountType;
 import banque.domain.repository.CompteRepository;
+import banque.infrastructure.config.GlobalValue;
 import banque.presentation.dto.CompteDTO;
 
 public class CompteService {
@@ -14,13 +19,48 @@ public class CompteService {
 	}
 		
 	// TODO: Validations
-	public CompteDTO createAccount(CompteDTO compteDto) {
+	public void createAccount(CompteDTO compteDto) {
+		Compte compte = null;
+		try {
+			Compte oldCompte = this.compteRepository.findByCode(compteDto.getCode());
+			if(oldCompte != null) {
+				throw new DuplicateAccount();
+			}
+			compte = CompteMapper.toEntity(compteDto);
+			
+
+			
+			this.compteRepository.save(compte);
+			
+			
+		} catch (DuplicateAccount e) {
+			System.err.println(e.getMessage());
+		}
 		
-		Compte compte = CompteMapper.toEntity(compteDto);
+	}
+	
+	public Double calcInterest(String code) {
+		Double Interest = 0.0;
+		try {
+			Compte compte = this.compteRepository.findByCode(code);
+			
+			//System.out.print("DEBUG: " + compte);
+			
+			if(compte == null) {
+				throw new CompteNotFound();
+			}
+			
+			if(!(compte instanceof CompteEpargne)) {
+				throw new InvalidAccountType();
+			}
+			
+			Interest =   compte.getSolde() * (100 / GlobalValue.TAUX_INTEREST);
+
+		} catch (CompteNotFound | InvalidAccountType e) {
+			System.err.println(e.getMessage());
+		}
 		
-		this.compteRepository.save(compte);
-		
-		return CompteMapper.toDTO(compte);
+		return Interest;
 	}
 	
 	
