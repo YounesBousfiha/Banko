e0fe43763f78f35c8cb72f92d35b44afa577d35a
Coderyounes <younesanas2021@gmail.com>
2025-09-16 10:48:39 +0100

    feat: Implement Repository Methods to CompteRepositoryMemory

--- src/banque/infrastructure/persistence/CompteRepositoryMemory.java
+++ src/banque/infrastructure/persistence/CompteRepositoryMemory.java
@@ -1,5 +1,24 @@
 package banque.infrastructure.persistence;
 
+import banque.domain.entity.Compte;
 import banque.domain.repository.CompteRepository;
+import java.util.HashMap;
+import java.util.Map;
 
-public class CompteRepositoryMemory implements CompteRepository {}
+public class CompteRepositoryMemory implements CompteRepository {
+	
+	private Map<String, Compte> comptes = new HashMap<>();
+	@Override
+	public void save(Compte compte) {
+		comptes.put(compte.getCode(), compte);
+	}
+
+	@Override
+	public Compte findByCode(String code) {
+		return comptes.get(code);
+	}
+
+	@Override
+	public boolean exists(String code) {
+		return comptes.containsKey(code);
+	}}
