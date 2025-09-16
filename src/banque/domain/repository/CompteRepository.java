50f3b376e389a9adeea451e0881ac239740ff8bd
Coderyounes <younesanas2021@gmail.com>
2025-09-16 19:47:07 +0100

    chore: add comment for a future update

--- src/banque/domain/repository/CompteRepository.java
+++ src/banque/domain/repository/CompteRepository.java
@@ -5,5 +5,5 @@ import banque.domain.entity.Compte;
 public interface CompteRepository {
 	void save(Compte compte);
 	Compte findByCode(String code);
-	boolean exists(String code);
+	boolean exists(String code); // use this when Cheking for Account Existance
 }
