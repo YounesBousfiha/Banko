87ecdffd487cdfa969004ca0c3a7d9f4e4f7d375
Coderyounes <younesanas2021@gmail.com>
2025-09-16 19:03:10 +0100

    feat: add new method in OperationRepository & remove unused ones

--- src/banque/domain/repository/OperationRepository.java
+++ src/banque/domain/repository/OperationRepository.java
@@ -4,9 +4,6 @@ import banque.domain.entity.Operation;
 import java.util.List;
 
 public interface OperationRepository {
-	void save(Operation operation);
-	List<Operation> findAll();
-	//void virement(); // Move it to the Operation Services
-	//void depot(); // Move it to the Operation Services
-	//void retrait(); // Move it to the Operation Services
+	void save(Operation operation, String code);
+	List<Operation> findByCode(String code);
 }
