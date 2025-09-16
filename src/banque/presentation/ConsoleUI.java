e013eb06b192ce58a3696d032e60b99b7eb70acd
Coderyounes <younesanas2021@gmail.com>
2025-09-16 07:08:27 +0100

    feat: setup the ConsoleUI menus

--- src/banque/presentation/ConsoleUI.java
+++ src/banque/presentation/ConsoleUI.java
@@ -31,12 +31,15 @@ public class ConsoleUI {
 					effectuerVersement();
 					break;
 				case 3:
-					effectuerVirement();
+					effectuerRetrait();
 					break;
 				case 4:
-					consulterSolde();
+					effectuerVirement();
 					break;
 				case 5:
+					consulterSolde();
+					break;
+				case 6:
 					consulerOperations();
 					break;
 				case 0:
@@ -48,10 +51,67 @@ public class ConsoleUI {
 		} while( choice != 0);
 	}
 	
-	private void WelcomeMenu() {}
-	private void creeCompte() {}
-	private void effectuerVersement() {}
-	private void effectuerVirement() {}
-	private void consulterSolde() {}
-	private void consulerOperations() {}
+	private void WelcomeMenu() {
+        System.out.println("\n=== Banque Console ===");
+        System.out.println("1. Créer un compte (courant/épargne)");
+        System.out.println("2. Effectuer un versement");
+        System.out.println("3. Effectuer un retrait");
+        System.out.println("4. Effectuer un virement");
+        System.out.println("5. Consulter le solde");
+        System.out.println("6. Consulter la liste des opérations");
+        System.out.println("0. Quitter");
+        System.out.print("Votre choix : ");
+	}
+	
+	private void creeCompte() {
+		System.out.print("Code du Compte : "); // validation aprés
+		String code = scanner.nextLine();
+		System.out.print("Type (courant/epargne) : ");
+		String type = scanner.nextLine();
+		System.out.print("Solde initial : ");
+		double solde = scanner.nextDouble();
+		// Call CompteController to Create the Account
+	}
+	
+	private void effectuerVersement() {
+		System.out.print("Code du Compte : ");
+		String code = scanner.nextLine();
+		System.out.print("Montant : ");
+		double motant = scanner.nextDouble();
+		scanner.nextLine();
+		// Call controller to handle Versement(depot) 
+	}
+	
+	private void effectuerRetrait() {
+		System.out.print("Code du Compte : ");
+		String code = scanner.nextLine();
+		System.out.print("Montant : ");
+		double motant = scanner.nextDouble();
+		scanner.nextLine();
+		// Call controller to handle retrait
+	}
+	
+	private void effectuerVirement() {
+		System.out.print("Compte source : ");
+		String codesource = scanner.nextLine();
+		System.out.print("Compte destination : ");
+		String codeDest = scanner.nextLine();
+		System.out.print("Montant : ");
+		double montant = scanner.nextDouble();
+		scanner.nextLine();
+		// Call Virement Controller
+	}
+	
+	private void consulterSolde() {
+		System.out.print("Code du compte : ");
+		String code = scanner.nextLine();
+		// call comptecontroller.consulter solde & return data in double variable
+		System.out.println("Solde du compte "); // concatinate it with code & solde
+	}
+	
+	private void consulerOperations() {
+		System.out.println("Code du compte : ");
+		String code = scanner.nextLine();
+		// Call compteController.consulerOperations with code & return List of Operation 
+	}
 }
