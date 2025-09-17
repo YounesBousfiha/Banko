ce76a0f4aa190195b029184e38f598d84c9796ef
Coderyounes <younesanas2021@gmail.com>
2025-09-17 07:02:26 +0100

    feat: add new field to GlobalValue class

--- src/banque/infrastructure/config/GlobalValue.java
+++ src/banque/infrastructure/config/GlobalValue.java
@@ -4,4 +4,10 @@ public class GlobalValue {
 	
 	public static final Double TAUX_INTEREST = 2.00;
 	public static final Double DECOUVERT = 200.00;
+	
+	public static final boolean USE_DATABASE = false;
+	
+	public static final String URL = "jdbc:postgresql://localhost:5432/banko";
+	public static final String USER = "younes";
+	public static final String PASSWORD = "test123";
 }
